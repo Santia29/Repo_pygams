76519cd2ce1d71ff93c6b47bbf4d6af953b0a111
Santia29 <anllosantiago@hotmail.com>
2023-11-16 20:53:30 -0300

    primeros cambios

--- main.py
+++ main.py
@@ -5,48 +5,51 @@ from models.constantes import (
 )
 from models.player.main_player import Jugador
 
+from models.enemigo.main_enemigo import Enemigo
+
 screen = pg.display.set_mode((ANCHO_VENTANA, ALTO_VENTANA))
 pg.init()
 clock = pg.time.Clock()
 
-back_img = pg.image.load('./assets/img/background/goku_house.png')
+back_img = pg.image.load('./assets/img/background/the_simpson_background.png')
 back_img = pg.transform.scale(back_img, (ANCHO_VENTANA, ALTO_VENTANA))
 
 
 juego_ejecutandose = True
 
-vegeta = Jugador(0, 0, frame_rate=70, speed_walk=20, speed_run=40, gravity=10, jump=32)
-
+vegeta = Jugador(0, 1, frame_rate=70, speed_walk=20, speed_run=40, gravity=10, jump=32)
+enemigo_1 = Enemigo(0, 0.1, frame_rate=70, speed_walk=20, speed_run=40, gravity=0, jump=32)
 
 while juego_ejecutandose:
     
     #print(delta_ms)
     lista_eventos = pg.event.get()
     for event in lista_eventos:
-        match event.type:
-            case pg.KEYDOWN:
-                if event.key == pg.K_SPACE:
-                    vegeta.jump(True)
-            case pg.KEYUP:
-                if event.key == pg.K_SPACE:
-                    vegeta.stay()
-            #         print('Estoy SOLTANDO el espacio')
-            case pg.QUIT:
-                print('Estoy CERRANDO el JUEGO')
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_SPACE:
+                vegeta.jump(True)
+
+        if event.type == pg.KEYUP:
+            if event.key == pg.K_SPACE:
+                vegeta.jump(False)        
+            
+        if event.type == pg.KEYDOWN:    
+            if event.key == pg.K_ESCAPE:
                 juego_ejecutandose = False
                 break
     
-    lista_teclas_presionadas = pg.key.get_pressed()
-    if lista_teclas_presionadas[pg.K_RIGHT] and not lista_teclas_presionadas[pg.K_LEFT]:
+    tecla_presionada = pg.key.get_pressed()
+    if tecla_presionada[pg.K_RIGHT] and not tecla_presionada[pg.K_LEFT]:
         vegeta.walk('Right')
-    if lista_teclas_presionadas[pg.K_LEFT] and not lista_teclas_presionadas[pg.K_RIGHT]:
+    if tecla_presionada[pg.K_LEFT] and not tecla_presionada[pg.K_RIGHT]:
         vegeta.walk('Left')
-    if not lista_teclas_presionadas[pg.K_RIGHT] and not lista_teclas_presionadas[pg.K_LEFT] and not lista_teclas_presionadas[pg.K_SPACE]:
+
+    if not tecla_presionada[pg.K_RIGHT] and not tecla_presionada[pg.K_LEFT] and not tecla_presionada[pg.K_SPACE]:
         vegeta.stay()
     
-    if lista_teclas_presionadas[pg.K_RIGHT] and lista_teclas_presionadas[pg.K_LSHIFT] and not lista_teclas_presionadas[pg.K_LEFT]:
+    if tecla_presionada[pg.K_RIGHT] and tecla_presionada[pg.K_LSHIFT] and not tecla_presionada[pg.K_LEFT]:
         vegeta.run('Right')
-    if lista_teclas_presionadas[pg.K_LEFT] and lista_teclas_presionadas[pg.K_LSHIFT] and not lista_teclas_presionadas[pg.K_RIGHT]:
+    if tecla_presionada[pg.K_LEFT] and tecla_presionada[pg.K_LSHIFT] and not tecla_presionada[pg.K_RIGHT]:
         vegeta.run('Left')
     
         
@@ -55,6 +58,9 @@ while juego_ejecutandose:
     delta_ms = clock.tick(FPS)
     vegeta.update(delta_ms)
     vegeta.draw(screen)
+    
+    enemigo_1.update(delta_ms)
+    enemigo_1.draw(screen)
     pg.display.update()
 
 pg.quit()
